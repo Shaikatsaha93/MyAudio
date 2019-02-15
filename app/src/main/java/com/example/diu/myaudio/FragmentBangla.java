0261607d77f836955f1e78eb5a2cd0b99b0fbcc7
Shaikat Saha <shaikatsaha93@gmail.com>
2019-02-15 17:39:11 +0600

    first

---
+++ app/src/main/java/com/example/diu/myaudio/FragmentBangla.java
@@ -0,0 +1,24 @@
+package com.example.diu.myaudio;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentBangla extends Fragment {
+
+    View v;
+
+    public FragmentBangla() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.bangla_fragment,container,false);
+        return v;
+    }
+}

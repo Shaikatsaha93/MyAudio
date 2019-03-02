37060ec0339d871cd9bbf85a8004c1f8eb6ffeb0
Shaikat Saha <shaikatsaha93@gmail.com>
2019-03-02 23:01:42 +0600

    audio book

--- app/src/main/java/com/example/diu/myaudio/FragmentBangla.java
+++ app/src/main/java/com/example/diu/myaudio/FragmentBangla.java
@@ -1,16 +1,38 @@
 package com.example.diu.myaudio;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FragmentBangla extends Fragment {
 
     View v;
+    private static final String URL_DATA = "http://golposuni.arif-ahmed.com/api/story_list?fbclid=IwAR0gp4oysa4gPYIuVQFT3agSNOwWTqvkj2f2eixZDk_mEyN6BwqkTorOYHY";
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+
+    private List<BanglaItem> banglaItems;
 
     public FragmentBangla() {
     }
@@ -19,6 +41,65 @@ public class FragmentBangla extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.bangla_fragment,container,false);
+
+
+        recyclerView = (RecyclerView) v.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setHasFixedSize(true);
+
+        loadBanglaRecyclerViewData();
+
         return v;
+    }
+    private void loadBanglaRecyclerViewData(){
+        final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Loading Data...");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL_DATA,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            JSONArray array = jsonObject.getJSONArray("data");
+
+                            for (int i = 0; i<array.length(); i++){
+                                JSONObject o = array.getJSONObject(i);
+                                BanglaItem bangla = new BanglaItem(
+                                        o.getString("story_name"),
+                                        o.getString("writter_name"),
+                                        o.getString("story_image")
+                                );
+                                banglaItems.add(bangla);
+                            }
+
+                            adapter = new BanglaAdapter(banglaItems, getActivity().getApplicationContext());
+                            recyclerView.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        banglaItems = new ArrayList<>();
     }
 }

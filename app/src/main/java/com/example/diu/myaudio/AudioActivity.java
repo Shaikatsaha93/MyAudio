76d7a343bdbd9a0eb4a9521bd72fef31824ee31f
Shaikat Saha <shaikatsaha93@gmail.com>
2019-04-12 15:37:12 +0600

    audio design puse

--- app/src/main/java/com/example/diu/myaudio/AudioActivity.java
+++ app/src/main/java/com/example/diu/myaudio/AudioActivity.java
@@ -2,14 +2,28 @@ package com.example.diu.myaudio;
 
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
 public class AudioActivity extends AppCompatActivity {
+
+    private Button backBtn, forBtn;
+    private ImageButton playBtn;
+    private SeekBar seekbar;
+    private Runnable runnable;
+    private Handler handler;
+    int totaltime;
+
+    MediaPlayer player;
 
     TextView storyname, writername;
     ImageView image;
@@ -20,6 +34,14 @@ public class AudioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio);
+
+        backBtn = findViewById(R.id.backbtn);
+        playBtn = findViewById(R.id.playbtn);
+        forBtn = findViewById(R.id.forbtn);
+        handler = new Handler();
+        seekbar = findViewById(R.id.seekbar);
+        seekbar.setMax(totaltime);
+
 
         storyname = findViewById(R.id.audioStoryName);
         writername = findViewById(R.id.audioWriterName);
@@ -34,11 +56,25 @@ public class AudioActivity extends AppCompatActivity {
         writername.setText(writerStory);
         Picasso.get().load(imageStory).fit().into(image);
         try{
-            MediaPlayer player=new MediaPlayer();
+            player=new MediaPlayer();
             player.setAudioStreamType(AudioManager.STREAM_MUSIC);
             player.setDataSource(link);
             player.prepare();
             player.start();
+            playBtn.setBackgroundResource(R.drawable.ic_pause_black_24dp);
+            playBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (!player.isPlaying()){
+                        player.start();
+                        playBtn.setBackgroundResource(R.drawable.ic_pause_black_24dp);
+                    }else {
+                        player.pause();
+                        playBtn.setBackgroundResource(R.drawable.ic_play_arrow_black_24dp);
+                    }
+                }
+            });
+            totaltime = player.getDuration();
         }catch(Exception e){
 
         }
